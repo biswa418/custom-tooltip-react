dc09c7d0cc78c12774d6bcd37594bd0a8824d0be
Biswajeet Sutar <88776869+biswa418@users.noreply.github.com>
2023-05-29 20:51:38 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import Tooltip from './Tooltip'
+import { useState } from "react";
+import Tooltip from "./Tooltip";
 
 function App() {
   const [hovering, setHovering] = useState(false);
 
   function handleMouseOver() {
-    setHovering(true)
+    setHovering(true);
   }
 
   function handleMouseLeave() {
@@ -14,14 +14,15 @@ function App() {
 
   return (
     <>
-      <div className='container'>
-        {hovering && <Tooltip position='top' />}
+      <div className="container">
+        {/* Change the prop here to see the effect */}
+        {hovering && <Tooltip position="top" />}
         <button onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
           Hover over me!
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
